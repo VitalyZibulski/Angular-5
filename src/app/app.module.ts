cb717ac9dbd1a6bef6d6ab1c0a61e572ed6fd437
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2018-10-05 19:09:13 +0300

    create component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MyfirstComponent } from './components/myfirstcomponent/myfirstcomponent.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyfirstComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/myfirstcomponent/myfirstcomponent.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector:'app-myfirstcomponent',
+    templateUrl:'./myfirstcomponent.component.html',
+    styleUrls:['./myfirstcomponent.component.css']
+})
+
+export class MyfirstComponent {
+
+}
