81d14c7ccb46d6e85b6025f001f721c1309bc008
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2018-10-16 00:36:45 +0300

    use another style

--- src/app/car/car.component.ts
+++ src/app/car/car.component.ts
@@ -5,7 +5,12 @@ import { Component, OnInit } from '@angular/core';
   template: `
     <h2>Single car</h2>
   `,
-  styleUrls: ['./car.component.css']
+  // styleUrls: ['./car.component.css']
+    styles:[`
+        h2 {
+            color:aqua;
+        }
+    `]
 })
 export class CarComponent implements OnInit {
 
