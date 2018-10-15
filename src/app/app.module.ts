5503ab54b68e2cca6a3b209875a1c4ba8c1c981b
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2018-10-16 00:17:26 +0300

    create component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { MyComponent } from './my/my.component'; //class export
 
 @NgModule({
   declarations: [
     AppComponent,
+    MyComponent
   ],
   imports: [
     BrowserModule
