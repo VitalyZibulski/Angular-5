05db54253d410cf74605d976075e10f2c6c625ad
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2018-10-16 00:30:22 +0300

    component in component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { MyComponent } from './my/my.component';
-import { My2Component } from './my2/my2.component'; //class export
+import { CarsComponent } from './cars/cars.component';
+import { CarComponent } from './car/car.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyComponent,
-    My2Component
+    CarsComponent,
+    CarComponent,
   ],
   imports: [
     BrowserModule
