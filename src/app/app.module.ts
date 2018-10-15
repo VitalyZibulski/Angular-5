2a21f3461a8bdbf7169a6b7446a81b39203d5e5f
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2018-10-15 23:30:32 +0300

    fix project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { MyfirstComponent } from './components/myfirstcomponent/myfirstcomponent.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyfirstComponent
   ],
   imports: [
     BrowserModule
